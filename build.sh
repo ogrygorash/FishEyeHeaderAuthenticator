4685b8c910918a065e458b7ffef9eeda47231cc3
ogrygorash <28525743+ogrygorash@users.noreply.github.com>
2020-05-07 18:46:17 -0700

    Update destination path.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 rm -f com/cenqua/fisheye/user/plugin/*.class
 javac -cp "/home/crucible/fecru/lib/*" com/cenqua/fisheye/user/plugin/FishEyeHeaderAuthenticator.java 
 jar cf FishEyeHeaderAuthenticator.jar com/cenqua/fisheye/user/plugin/*.class
-cp FishEyeHeaderAuthenticator.jar /home/crucible/fecru/lib/
+cp FishEyeHeaderAuthenticator.jar /home/crucible/crucible/lib/

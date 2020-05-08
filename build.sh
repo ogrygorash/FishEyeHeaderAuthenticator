2acab863fa19743513168229f7e4fb35b775402e
ogrygorash <28525743+ogrygorash@users.noreply.github.com>
2020-05-07 18:41:56 -0700

    Update build paths.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm com/cenqua/fisheye/user/plugin/*.class
-javac -cp "/opt/atlassian/fisheye/lib/*" com/cenqua/fisheye/user/plugin/FishEyeHeaderAuthenticator.java 
+javac -cp "/home/crucible/fecru/lib/*" com/cenqua/fisheye/user/plugin/FishEyeHeaderAuthenticator.java 
 jar cf FishEyeHeaderAuthenticator.jar com/cenqua/fisheye/user/plugin/*.class
-cp FishEyeHeaderAuthenticator.jar /var/atlassian/fisheye/lib/
+cp FishEyeHeaderAuthenticator.jar /home/crucible/fecru/lib/
